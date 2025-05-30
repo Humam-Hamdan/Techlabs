3739168548470d89c8c3a610cadfde3227aad0a9
HumHad <58117805+Humam-Hamdan@users.noreply.github.com>
2025-05-30 17:13:31 +0200

    couple of edits

--- DataScience-du/01_Introduction to Python Programming/chapter-01.py
+++
@@ -1,9 +0,0 @@
-
-# 1. Write a program to prompt the user for hours and rate per hour using input to compute gross pay. Use 35 hours and a rate of 2.75 per hour to test the program (the pay should be 96.25). You should use input to read a string and float() to convert the string to a number. Do not worry about error checking or bad user data. Print your computed pay.
-# Use the built-in input method to read hours and rate per hour separately.
-# Typecast both inputs to floats.
-# Print the result by multiplying the hours with their rate per hour using the *-operator.
-
-hours = float(input("How many hours?\n"))
-rate = float(input("What is the hourly rate?\n"))
-print("The pay is", hours * rate)
